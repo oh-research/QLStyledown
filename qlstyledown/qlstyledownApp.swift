19252719035560cc009162a630662a757743a80d
Semin Oh <osmfirst@gmail.com>
2026-03-18 21:30:00 +0900

    Initial Commit

---
+++ qlstyledown/qlstyledownApp.swift
@@ -0,0 +1,17 @@
+//
+//  qlstyledownApp.swift
+//  qlstyledown
+//
+//  Created by SeminOH on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct qlstyledownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
